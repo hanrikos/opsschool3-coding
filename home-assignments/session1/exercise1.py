9d9856338bcebdfff79924f8555de8ef46dff500
hanrikos <hanrikos@gmail.com>
2018-11-02 20:56:30 +0200

    a few try/expects and dynamic bucket logic were added

--- home-assignments/session1/exercise1.py
+++ home-assignments/session1/exercise1.py
@@ -1,6 +1,6 @@
 import json
 from collections import defaultdict
-
+import operator
 
 INPUT_JSON_PATH = "/home/hans/opsschool/opsschool3-coding/home-assignments/session1/input_file.json"
 OUTPUT_DIRECTORY = "/home/hans/opsschool/opsschool3-coding/home-assignments/session1/"
@@ -10,49 +10,67 @@ def load_json_file(json_file):
     :param json_file: jason file path
     :return: the content of the json file
     """
-    with open(json_file, "r") as d:
-        data = json.load(d)
-        return data
+    try:
+        with open(json_file, "r") as d:
+            data = json.load(d)
+            return data
+
+    except Exception as e:
+        print(e)
 
 
-def group_by_age(json_content):
+def get_ages_and_bucket_ranges(json_content):
     """
     :param json_content: json_content
     :return: list of people grouped by their ages
     """
+    #  people age list
     d = json_content["ppl_ages"]
+    sorted_by_value = sorted(d.items(), key=lambda kv: kv[1])
+    last_age = sorted_by_value[-1]
 
-    range_one = []  # 11 to 20
-    range_two = []  # 20 to 25
-    range_three = []  # 25 to 40
-    range_four = []  # 40 to 102
-    out_of_ranges = []  # out of range
+    #  sorted bucket list
+    b = json_content["buckets"]
+    b.append(last_age[1])
+    b = [int(x) for x in b]
+    b.sort()
 
-    for key, value in d.items():
-        if 11 < value < 20:
-            range_one.append(key)
-        if 20 < value < 25:
-            range_two.append(key)
-        if 25 < value < 40:
-            range_three.append(key)
-        if 40 < value < 102:
-            range_four.append(key)
-        else:
-            out_of_ranges.append(key)
+    dynamic_bucket_list = list(zip(b, b[1:]))
+    min_bucket = b[0]
+    max_bucket = b[-1]
+    ages_dict = d
 
-    return range_one, range_two, range_three, range_four, out_of_ranges
+    return ages_dict, dynamic_bucket_list, min_bucket, max_bucket
 
 
-def append_multiple_jsons(range_one, range_two, range_three, range_four, out_of_ranges):
+def create_yaml_based_on_bucket_ranges(ages_dict, dynamic_bucket_list, min_bucket, max_bucket):
     """
-    :return: A new json file with people grouped by their ages
+    :return: A new yaml file with people grouped by dynamic buckets
     """
+
     output_list = dict()
-    output_list["11 to 20"] = range_one
-    output_list["20 to 25"] = range_two
-    output_list["25 to 40"] = range_three
-    output_list["40 to 102"] = range_four
-    output_list["Out of range"] = out_of_ranges
+
+    for key, value in dynamic_bucket_list:
+        list_names = "{} - {}".format(key, value)
+        output_list[list_names] = []
+
+    for key, value in dynamic_bucket_list:
+        for key1, value1 in ages_dict.items():
+            if str(key1) not in str(output_list):
+                if key < value1 < value:
+                    list_name = (str(key) + " - " + str(value))
+                    output_list[str(list_name)] += [key1]
+
+                if value1 >= max_bucket or value1 <= min_bucket:
+                    out_list_name = "out of range"
+                    if out_list_name in output_list:
+                        [out_list_name].append(key1)
+                        output_list[str(out_list_name)] += [key1]
+                    else:
+                        output_list[out_list_name] = []
+                        [out_list_name].append(key1)
+                        output_list[str(out_list_name)] += [key1]
+
 
     with open("{}output_file.yaml".format(OUTPUT_DIRECTORY), "w") as outfile:
         json.dump(output_list, outfile)
@@ -63,10 +81,8 @@ def main():
     print("loading the json file")
     json_content = load_json_file(INPUT_JSON_PATH)
     print("Listing people by ages")
-    range_one, range_two, range_three, range_four, out_of_ranges = group_by_age(json_content)
-    print(range_one, range_two, range_three, range_four, out_of_ranges)
-    print("Creating new Json")
-    append_multiple_jsons(range_one, range_two, range_three, range_four, out_of_ranges)
+    ages_dict, dynamic_bucket_list, min_bucket, max_bucket = get_ages_and_bucket_ranges(json_content)
+    create_yaml_based_on_bucket_ranges(ages_dict, dynamic_bucket_list, min_bucket, max_bucket)
 
 
 if __name__ == "__main__":
