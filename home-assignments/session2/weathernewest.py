606038840b2abde04fcabfa783d0aaa08349a94a
hanrikos <hanrikos@gmail.com>
2018-11-08 21:24:21 +0200

    class weather exercise is completed

--- home-assignments/session2/weathernewest.py
+++ home-assignments/session2/weathernewest.py
@@ -1,26 +1,21 @@
 import sys
-import datetime
 from weather import Weather, Unit
+
 
 def main(city_name):
     weather = Weather(unit=Unit.CELSIUS)
-
     location = weather.lookup_by_location(city_name)
     forecasts = location.forecast
-    today = datetime.datetime.today().strftime('%d %m %Y')
+    weather_condition = forecasts[0].text
+    max_temperature = forecasts[0].high
+    min_temperature = forecasts[0].low
+    forecast_date = forecasts[0].date
 
-    for forecast in forecasts:
-        city_weather_condition = forecast.text
-
-        max_temperature = forecast.high
-        min_temperature = forecast.low
-        if forecast.date == today:
-            break
-    print("The weather in {} today is {} with temperatures trailing from {}-{} celsius".format(city_name, city_weather_condition, min_temperature, max_temperature ))
+    print("The weather in {} today({}) is {} with temperatures trailing from {}-{} celsius.".format(city_name, forecast_date, weather_condition, min_temperature, max_temperature))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(f"Please add city namet: {sys.argv[0]} <city_name>")
+        print(f"Please enter city name: {sys.argv[0]} <city_name>")
     else:
         main(sys.argv[1])
